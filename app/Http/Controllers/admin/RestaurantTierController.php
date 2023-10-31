e0eeb44f3676bf3fa07f22b3b25990af05314d34
amir aghighi <aghighiamir@yahoo.com>
2023-10-31 17:34:26 +0330

    add: add resourse controllers foق or phaze 1

---
+++ app/Http/Controllers/admin/RestaurantTierController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\RestaurantTier;
+use Illuminate\Http\Request;
+
+class RestaurantTierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RestaurantTier $restaurantTier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RestaurantTier $restaurantTier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RestaurantTier $restaurantTier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RestaurantTier $restaurantTier)
+    {
+        //
+    }
+}
