ae808c11bb02704cbff6c738b415467d90e81104
amir aghighi <aghighiamir@yahoo.com>
2023-12-03 22:12:03 +0330

    fix: fix store address request

--- app/Http/Requests/api/StoreAddressRequest.php
+++ app/Http/Requests/api/StoreAddressRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\api;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 
 class StoreAddressRequest extends FormRequest
 {
@@ -23,16 +22,12 @@ class StoreAddressRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'x' => ['required','decimal:0,6'],
-            'y' => ['required','decimal:0,6'],
+            'lang' => ['required', 'decimal:0,6'],
+            'long' => ['required', 'decimal:0,6'],
             'address' => ['required', 'string', 'between:5,255'],
             'vahed' => ['required', 'numeric', 'between:1,200'],
-            'user_id' => ['required', 'exists:user,id']
+            'title' => ['required', 'string', 'between:3,50']
         ];
     }
 
-    protected function prepareForValidation()
-    {
-        $this->merge(['user_id' => Auth::id()]);
-    }
 }
