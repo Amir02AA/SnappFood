eae89c530ebfa3f9efab1a84b9c004538f56db0f
amir aghighi <aghighiamir@yahoo.com>
2023-12-03 17:36:58 +0330

    fix: show errors in profile page for salesMan

--- resources/views/sales/profile.blade.php
+++ resources/views/sales/profile.blade.php
@@ -12,21 +12,26 @@
                 <div class="w-full border-b-2 text-lg duration-300 focus-within:border-indigo-500">
                     <input type="text" name="name" placeholder="Name" class="w-full border-none bg-transparent outline-none placeholder-italic focus:outline-none text-white">
                 </div>
+                @error('name') {{$message}} @enderror
                 <div class="w-full border-b-2 text-lg duration-300 focus-within:border-indigo-500">
                     <input type="text" name="phone" placeholder="Phone" class="w-full border-none bg-transparent outline-none placeholder-italic focus:outline-none text-white">
                 </div>
+                @error('phone') {{$message}} @enderror
                 <div class="w-full text-lg duration-300">
                     <div class="w-full border-b-2 text-lg duration-300 focus-within:border-indigo-500 mb-2">
                         <textarea type="text" name="address" placeholder="Address" class="w-full border-none bg-transparent outline-none placeholder-italic focus:outline-none text-white"></textarea>
                     </div>
                     <div class="flex gap-1.5 w-full  text-lg duration-300 ">
-                        <input type="text" name="lang" placeholder="Langitude" class="w-full border-b-2 focus-within:border-indigo-500 bg-transparent outline-none placeholder-italic focus:outline-none text-white"></input>
+                        <input type="text" name="lang" placeholder="Latitude" class="w-full border-b-2 focus-within:border-indigo-500 bg-transparent outline-none placeholder-italic focus:outline-none text-white">
+                        @error('lang') {{$message}} @enderror
                         <input type="text" name="long" placeholder="Longitude" class="w-full border-b-2 focus-within:border-indigo-500 bg-transparent outline-none placeholder-italic focus:outline-none text-white">
+                        @error('long') {{$message}} @enderror
                     </div>
 
                 </div>
                 <div class="w-full border-b-2 text-lg duration-300 focus-within:border-indigo-500">
                     <input type="text" name="account" placeholder="Account Number" class="w-full border-none bg-transparent outline-none placeholder-italic focus:outline-none text-white">
+                    @error('account') {{$message}} @enderror
                 </div>
                 <div>
                     @foreach($tiers as $tier)
@@ -35,6 +40,7 @@
                             <input type="checkbox" name="tiers[]" id="type{{$tier->id}}" value="{{$tier->id}}">
                         </div>
                     @endforeach
+                        @error('tiers') {{$message}} @enderror
                 </div>
                 <div class="w-full">
                     <button type="submit" class="w-full transform rounded-sm bg-pink-600 py-2 font-bold duration-300 hover:bg-pink-400"> Add Food </button>
